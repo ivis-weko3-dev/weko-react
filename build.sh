488557e621b9ba88ebd6d355e9afee5db3433771
wei-kuochen <wei_kuochen@co-graph.co.jp>
2022-01-27 06:35:24 +0000

    weko#29913 added shell script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# quit on errors and unbound symbols:
+set -o errexit
+
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+
+# build-begin
+while read dir
+do
+    ( cd "${CURRENT_DIR}/$dir"; npm install )
+    ( cd "${CURRENT_DIR}/$dir"; npm run build )
+done <<END
+app-facet-search
+END
+# build-end
---
+++ cp_app_facet_search.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# quit on errors and unbound symbols:
+set -o errexit
+
+# args-check-begin
+if [ ! -d "$1" ]; then
+    echo "Usage: $0 wekodir"
+    exit 1
+fi
+WEKODIR=$1
+
+if [ ! -d "${WEKODIR}/modules" ]; then
+    echo "No such ${WEKODIR}/modules/"
+    exit 1
+fi
+TARGETDIR=$WEKODIR/modules
+# args-check-end
+
+# copy-begin
+cp -p ./app-facet-search/build/static/css/*.chunk.css      ${TARGETDIR}/weko-search-ui/weko_search_ui/static/css/weko_search_ui/facet_chunk.css
+cp -p ./app-facet-search/build/static/js/main.*.chunk.js   ${TARGETDIR}/weko-search-ui/weko_search_ui/static/js/weko_search_ui/facet.main.chunk.js
+cp -p ./app-facet-search/build/static/js/runtime-main.*.js ${TARGETDIR}/weko-search-ui/weko_search_ui/static/js/weko_search_ui/facet.runtime-main.js
+cp -p ./app-facet-search/build/static/js/2.*.chunk.js      ${TARGETDIR}/weko-search-ui/weko_search_ui/static/js/weko_search_ui/facet.chunk.js
+# copy-end
